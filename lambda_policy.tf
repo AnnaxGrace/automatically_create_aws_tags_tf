892ea9d329df48f12983e197184aac7c291797d4
annaxgrace <anna.grace.conover@gmail.com>
2023-03-31 18:28:23 +0100

    removed unecessary permissions from policy

--- lambda_policy.tf
+++ lambda_policy.tf
@@ -2,14 +2,11 @@ resource "aws_iam_policy" "auto_tag_lambda" {
   name        = "${var.name}-lambda"
   description = "Policy that allows lambda to auto create tags for newly created resources"
 
-  #ANNA variabilize this policy
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
   policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
       {
-        "Sid" : "VisualEditor0",
+        "Sid" : "CloudWatchMetrics",
         "Effect" : "Allow",
         "Action" : [
           "cloudwatch:PutMetricData",
@@ -17,7 +14,7 @@ resource "aws_iam_policy" "auto_tag_lambda" {
         "Resource" : "*"
       },
       {
-        "Sid" : "VisualEditor1",
+        "Sid" : "GlobalPermissions",
         "Effect" : "Allow",
         "Action" : [
           "logs:DescribeLogGroups",
@@ -32,19 +29,16 @@ resource "aws_iam_policy" "auto_tag_lambda" {
         ]
       },
       {
-        "Sid" : "VisualEditor2",
+        "Sid" : "s3Tagging",
         "Effect" : "Allow",
         "Action" : [
-          "logs:CreateLogStream",
           "s3:PutBucketTagging",
-          "s3:GetBucketTagging",
-          "logs:CreateLogGroup",
-          "logs:PutLogEvents"
+          "s3:GetBucketTagging"
         ],
         "Resource" : [
           "arn:aws:s3:::*",
-          "arn:aws:logs:us-east-1:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${var.name}",
-          "arn:aws:logs:us-east-1:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${var.name}:log-stream:*"
+          # "arn:aws:logs:us-east-1:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${var.name}",
+          # "arn:aws:logs:us-east-1:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${var.name}:log-stream:*"
         ]
       }
     ]
